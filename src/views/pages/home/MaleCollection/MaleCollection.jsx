75fbc6fa023fa4fceef68294c3e7d0c847ceb533
dotman/ <adedotunabimbola2@gmail.com>
2024-02-07 15:44:53 +0100

    Update MaleCollection.jsx

--- src/views/pages/home/MaleCollection/MaleCollection.jsx
+++ src/views/pages/home/MaleCollection/MaleCollection.jsx
@@ -17,7 +17,7 @@ const MaleCollection = () => {
 				<div className="products">
 					<div className="spec-col">
 						<div className="text-arrangement">
-							<p>TOP COLLECTIONS 2022 </p>
+							<p>TOP COLLECTIONS </p>
 							<h4>Up to 50% discount </h4>
 						</div>
 						<CollectionCard />
